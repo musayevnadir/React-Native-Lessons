175ea0cfecbbda45a5349a6a2d112838b8b6a5c0
musayevnadir <msyv.nadir@gmail.com>
2024-01-21 15:33:27 +0400

    Task:Enhanced JavaScript Data Types,Operators,and Object Practice

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /** @format */
 
+// ! Task 1
+
 // Part 1
 
 const numbers = [10, 20, 30, 40, 50];
@@ -54,3 +56,51 @@ console.log("isEvenAndPositive:", isEitherEvenAndPositive);
 
 const isNotNegative = firstNumber >= 0;
 console.log("isNotNegative:", isNotNegative);
+
+console.log("-------------------------------------------------------------");
+// ------------------------------------------------------------------------
+
+// ! Task 2
+
+// Part 1
+
+const scores = [213, 345, 564, 754, 756];
+
+const [firstScore, secondScore] = scores;
+console.log("FirstScore:", firstScore);
+console.log("SecondScore:", secondScore);
+
+const student = {
+  name: "Nadir",
+  age: 33,
+  isEnrolled: false,
+};
+
+const { name, age } = student;
+
+console.log("Name:", name);
+console.log("Age:", age);
+
+// Part 2
+
+console.log("TypeOf Scores:", typeof scores);
+console.log("TypeOf Name:", typeof name);
+console.log("TypeOf Age", typeof age);
+console.log("Scores Length:", scores.length);
+
+// Part 2
+
+console.log("Sum:", firstScore + secondScore);
+console.log("Difference::", firstScore - secondScore);
+
+console.log("Compare:", firstScore > secondScore);
+console.log("Compare:", firstScore < secondScore);
+
+console.log(firstScore > secondScore && secondScore > age);
+console.log(firstScore < secondScore || secondScore > age);
+
+if (!student.isEnrolled) {
+  console.log("Student is not enrolled");
+} else {
+  console.log("Student is enrolled");
+}
